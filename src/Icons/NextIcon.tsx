b7119c2f5ea0fc51514a9c5b822b9c3b73ded7cb
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-08-08 16:40:11 -0500

    fix icon

--- src/Icons/NextIcon.tsx
+++ src/Icons/NextIcon.tsx
@@ -8,8 +8,8 @@ interface NextIconProps extends SVGProps<SVGSVGElement> {
 export const NextIcon: FC<NextIconProps> = ({ heigth, width, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={heigth}
-    height={width}
+    width={width}
+    height={heigth}
     fill="none"
     viewBox="0 0 24 24"
     {...props}
