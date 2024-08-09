e9f24853b38669003a8d13de8837f9d61fb43c24
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-08-09 01:58:34 -0500

    new Icon

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ export { ArrowLeft } from "./Icons/ArrowRigth";
 export { NextIcon } from "./Icons/NextIcon";
 export { PreviousIcon } from "./Icons/PreviousIcon";
 export { GoogleIcon } from "./Icons/GoogleIcon";
+export { CopyIcon } from "./Icons/CopyIcon";
 //
 
 export { Menu } from "./Menu/Menu";
