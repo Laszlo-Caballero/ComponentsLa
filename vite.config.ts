f10fabde5b3f96807c5ae3e1b7d0d47f2dddce9a
laszlo-caballero <laszlocaballero@gmail.com>
2025-03-02 09:35:36 -0500

    fix(vite plugins): se arreglo los plugins de tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true }), tailwindcss()],
+  plugins: [tailwindcss(), react(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
