561257a1a051d7812cb3581936025ac8402f4597
laszlo-caballero <laszlocaballero@gmail.com>
2025-03-06 16:19:58 -0500

    feat(Box): agregar soporte para referencias usando forwardRef

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -1,25 +1,31 @@
-import { FC } from "react";
+import { FC, forwardRef } from "react";
 import { type BoxProps } from "./TypesBox";
 
-export const Box: FC<BoxProps> = ({
-  component: Component = "div",
-  className,
-  children,
-  height,
-  width,
-  display = "block",
-  alignItems = "start",
-  justifyContent = "start",
-  sx,
-  ...rest
-}) => {
-  return (
-    <Component
-      className={className}
-      style={{ height, width, display, alignItems, justifyContent, ...sx }}
-      {...rest}
-    >
-      {children}
-    </Component>
-  );
-};
+export const Box: FC<BoxProps> = forwardRef<HTMLDivElement, BoxProps>(
+  (
+    {
+      component: Component = "div",
+      className,
+      children,
+      height,
+      width,
+      display = "block",
+      alignItems = "start",
+      justifyContent = "start",
+      sx,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <Component
+        ref={ref}
+        className={className}
+        style={{ height, width, display, alignItems, justifyContent, ...sx }}
+        {...rest}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
